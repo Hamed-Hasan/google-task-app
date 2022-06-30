ef5f3b41a4d60c919412fd1dcdf8e51c44b63766
John Doe <hamedhasan422421@gmail.com>
2022-06-30 15:36:00 +0300

    setup route

--- src/Shared/NavBar.js
+++ src/Shared/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 const NavBar = () => {
     return (
         <div class="navbar bg-base-100 px-20">
@@ -9,9 +9,10 @@ const NavBar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
+        <li><NavLink to="task">Completed Task</NavLink></li>
+        <li><NavLink to="todo">Todo App</NavLink></li>
+        <li><NavLink to="calendar">Calendar</NavLink></li>
        
-        <li><a>Item 3</a></li>
       </ul>
     </div>
     <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
